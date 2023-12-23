78a9b97d2d8f085cd11ff9c2b9b2f1e2a57e7dd7
catofwind <134774746+catofwind@users.noreply.github.com>
2023-12-23 21:09:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import numpy as np
 from collections import defaultdict, deque
 from game import Board, Game
 from mcts_pure import MCTSPlayer as MCTS_Pure
-from mcts_alphaZero import MCTSPlayer
+from mcts_enhanced import MCTSPlayer
 from policy_value_net import PolicyValueNet  # tensorflow
 
 
